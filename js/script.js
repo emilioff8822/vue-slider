8ead38504d0b93bca0efe256ed62a40f9acf0916
emilioff8822 <emilioff@hotmail.it>
2023-04-13 14:21:05 +0200

    commit-slides-next-prev-h2

--- js/script.js
+++ js/script.js
@@ -14,6 +14,15 @@ pizze : [
 "img/cinque.jpg"
 
 ],  
+nomiPizze: [
+  "Pizza 1, diavola",
+  "Pizza 2, margherita",
+  "Pizza 3, marinara",
+  "Pizza 4, capricciosa",
+  "Pizza 5, con salsiccia"
+],
+
+
 infoPizze: {
 titolo: "Slides"
   
@@ -32,12 +41,14 @@ titolo: "Slides"
 
 methods: {
 
-  nextPrev(n) {
-    this.counter += n;
+  next() {
+    this.counter++;
     if (this.counter === this.pizze.length) this.counter = 0;
+  },
+  prev() {
+    this.counter--;
     if (this.counter < 0) this.counter = this.pizze.length - 1;
   }
-
 
 
 
